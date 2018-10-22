99209cbb48fd9eb04ab1cb9df48a0dba30afdb6b
hat2@students.ptcollege.edu <>
2018-10-22 15:30:45 -0400

    This is my first commit.

---
+++ src/thomas/Thomas.java
@@ -0,0 +1,14 @@
+package thomas;
+
+public class Thomas
+{
+    public static void main(String[] args)
+    {
+        Thomas();
+    }
+    
+    public static void Thomas()
+    {
+        System.out.println("Henry Thomas!!");
+    }
+}
